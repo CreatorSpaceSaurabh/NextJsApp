f89e3d5df7b20c4eb02f26d896f77c766bf51066
unknown <gokussjindia@gmail.com>
2024-05-27 23:52:01 +0530

    Home page- with gradient button effect added

--- nextjs-app/src/app/Home/page.tsx
+++ nextjs-app/src/app/Home/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const home = () => {
   return (
-    <div>home</div>
-  )
-}
+    <div class="min-h-screen bg-gray-50 flex flex-col justify-center relative overflow-hidden ">
+      <div class="max-w-7xl mx-auto">
+        <div class="relative group cursor-pointer">
+          <div class="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+          <div class="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+            <div class="space-y-2">
+              <p class="text-slate-800">Welcome to Home</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default home
+export default home;
