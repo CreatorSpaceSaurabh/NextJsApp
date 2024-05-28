ebe8d58fe92412eb4fbe36c545c2c5bbb72d5069
unknown <gokussjindia@gmail.com>
2024-05-28 23:48:30 +0530

    Dark mode toggle added for theme switch

--- nextjs-app/src/app/Home/page.tsx
+++ nextjs-app/src/app/Home/page.tsx
@@ -1,16 +1,59 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const home = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    console.log(isChecked);
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <div class="min-h-screen bg-gray-50 flex flex-col justify-center relative overflow-hidden ">
+    <div
+      class={`min-h-screen ${
+        !isChecked ? "bg-gray-50" : "bg-black-50"
+      }  flex flex-col justify-center relative overflow-hidden `}
+    >
       <div class="max-w-7xl mx-auto">
         <div class="relative group cursor-pointer">
           <div class="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
           <div class="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
             <div class="space-y-2">
-              <p class="text-slate-800">Welcome to Home</p>
+              <p class={` ${!isChecked ? "text-slate-800" : "text-slate-200"}`}>
+                Welcome to Home
+              </p>
             </div>
           </div>
+        </div>
+        <div className="mt-5">
+          <label className="themeSwitcherTwo relative inline-flex cursor-pointer select-none items-center">
+            <input
+              type="checkbox"
+              checked={isChecked}
+              defaultChecked={isChecked}
+              onClick={handleChange}
+              className="sr-only"
+            />
+            <span className="label flex items-center text-sm font-medium text-black">
+              Light mode
+            </span>
+            <span
+              className={`slider mx-4 flex h-8 w-[60px] items-center rounded-full p-1 duration-200 ${
+                isChecked ? "bg-[#212b36]" : "bg-[#CCCCCE]"
+              }`}
+            >
+              <span
+                className={`dot h-6 w-6 rounded-full bg-white duration-200 ${
+                  isChecked ? "translate-x-[28px]" : ""
+                }`}
+              ></span>
+            </span>
+            <span className="label flex items-center text-sm font-medium text-black">
+              Dark mode
+            </span>
+          </label>
         </div>
       </div>
     </div>
