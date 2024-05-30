a6091de95a8d9c4f8df7a4660cb937628d76b2c3
unknown <gokussjindia@gmail.com>
2024-05-30 22:22:58 +0530

    Service created for dynamic API calling - getAll and getDetails

---
+++ nextjs-app/src/app/Home/apiService.tsx
@@ -0,0 +1,19 @@
+export function getPokemon({ url }: any) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+}
+
+export async function getAllPokemon(url: string) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+}
