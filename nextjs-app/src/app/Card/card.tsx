0a661b7806103ab3fbc3121fe16ab2ee52847b71
unknown <gokussjindia@gmail.com>
2024-06-07 23:54:00 +0530

    Dynamic card component implementation with dynamic values

--- nextjs-app/src/app/Card/card.tsx
+++ nextjs-app/src/app/Card/card.tsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const card = ({ details }) => {
   return (
-    <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <div class="flex justify-end px-4 pt-4">
+    <div className="w-full max-w-sm m-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex justify-end px-4 pt-4">
         {/* <button
           id="dropdownButton"
           data-dropdown-toggle="dropdown"
-          class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+          className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
           type="button"
         >
-          <span class="sr-only">Open dropdown</span>
+          <span className="sr-only">Open dropdown</span>
           <svg
-            class="w-5 h-5"
+            className="w-5 h-5"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -24,13 +24,13 @@ const card = ({ details }) => {
         {/* <!-- Dropdown menu --> */}
         {/* <div
           id="dropdown"
-          class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+          className="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
         >
-          <ul class="py-2" aria-labelledby="dropdownButton">
+          <ul className="py-2" aria-labelledby="dropdownButton">
             <li>
               <a
                 href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
               >
                 Edit
               </a>
@@ -38,7 +38,7 @@ const card = ({ details }) => {
             <li>
               <a
                 href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
               >
                 Export Data
               </a>
@@ -46,7 +46,7 @@ const card = ({ details }) => {
             <li>
               <a
                 href="#"
-                class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
               >
                 Delete
               </a>
@@ -54,31 +54,45 @@ const card = ({ details }) => {
           </ul>
         </div> */}
       </div>
-      <div class="flex flex-col items-center pb-10">
+      <div className="flex flex-col items-center pb-10">
         <img
-          class="w-24 h-24 mb-3 rounded-full shadow-lg"
+          className="w-28 h-28 m-4 rounded-full shadow-lg"
           src={details.sprites.front_default}
           alt="pokemon"
         />
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
           {details?.name?.toString()?.charAt(0)?.toUpperCase() +
             details?.name?.toString()?.substring(1)}
         </h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">
+        <span className="text-sm text-gray-500 dark:text-gray-400">
           Exp: {details.base_experience}
         </span>
-        <div class="flex mt-4 md:mt-6">
+        <span className="text-sm text-gray-500 dark:text-gray-400">
+          Height: {details.height}
+        </span>
+        <span className="text-sm text-gray-500 dark:text-gray-400">
+          Weight: {details.weight}
+        </span>
+        <div className="flex mt-4 md:mt-6">
           <a
             href="#"
-            class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Add friend
+            {details.types[0]?.type?.name
+              ?.toString()
+              ?.charAt(0)
+              ?.toUpperCase() +
+              details.types[0]?.type?.name?.toString()?.substring(1)}
           </a>
           <a
             href="#"
-            class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
           >
-            Message
+            {details.moves[0]?.move?.name
+              ?.toString()
+              ?.charAt(0)
+              ?.toUpperCase() +
+              details.moves[0]?.move?.name?.toString()?.substring(1)}
           </a>
         </div>
       </div>
